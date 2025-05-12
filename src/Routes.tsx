369f138119f9d36723fe3053cab1dd0dd586fa95
th3N0m4d <edielton.estrela.dantas@sap.com>
2025-05-12 14:59:50 +0200

    feat: setup `react-router-dom`

---
+++ src/Routes.tsx
@@ -0,0 +1,16 @@
+// src/routes.tsx
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+// import Login from "./pages/Login";
+// import Dashboard from "./pages/Dashboard";
+// import NotFound from "./pages/NotFound";
+
+export default function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<Login />} />
+      {/* <Route path="/dashboard" element={<Dashboard />} /> */}
+      {/* <Route path="*" element={<NotFound />} /> */}
+    </Routes>
+  );
+}
