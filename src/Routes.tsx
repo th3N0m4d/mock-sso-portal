3dc0d938a7a14fb8dbc0fb7a6a75a9d77856248a
th3N0m4d <edielton.estrela.dantas@sap.com>
2025-05-13 15:00:26 +0200

    feat(Routes): setup routing system

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,16 +1,30 @@
 // src/routes.tsx
 import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
-// import Login from "./pages/Login";
-// import Dashboard from "./pages/Dashboard";
-// import NotFound from "./pages/NotFound";
+import NotFound from "./pages/NotFound";
+import Dashboard from "./pages/Dashboard";
+import { GuestRoute, ProtectedRoute } from "./components";
 
 export default function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<Login />} />
-      {/* <Route path="/dashboard" element={<Dashboard />} /> */}
-      {/* <Route path="*" element={<NotFound />} /> */}
+      <Route
+        path="/login"
+        element={
+          <GuestRoute>
+            <Login />
+          </GuestRoute>
+        }
+      />
+      <Route
+        path="/"
+        element={
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        }
+      />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
