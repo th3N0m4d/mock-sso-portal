460cca9b1a0ba5753519c82fbf17b43431ef0c99
th3N0m4d <edielton.estrela.dantas@sap.com>
2025-05-13 15:45:03 +0200

    chore: setup ci/cd

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const base = "/mock-sso-portal/";
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base,
   test: {
     globals: true,
     environment: "jsdom",
