06846a7bedcb59cd63ab54289cf5e342cb8538cb
th3N0m4d <edielton.estrela.dantas@sap.com>
2025-05-13 15:06:15 +0200

    feat(Dashboard): use `logout` from `useAuthStore`

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,7 +1,18 @@
+import { useAuthStore } from "../hooks";
+
 export default function Dashboard() {
+  const { logout } = useAuthStore();
+  const handleClick = (e: React.FormEvent) => {
+    e.preventDefault();
+    logout();
+  };
+
   return (
     <div className="d-flex flex-column align-items-center justify-content-center vh-100 text-center p-4">
       <h1 className="display-1 fw-bold text-primary">Home Page</h1>
+      <button type="submit" className="btn btn-primary" onClick={handleClick}>
+        Logout
+      </button>
     </div>
   );
 }
