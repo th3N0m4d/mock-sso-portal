747155f7e0bc27809e2fc8ecd821433d75dd839b
th3N0m4d <edielton.estrela.dantas@sap.com>
2025-05-29 17:23:41 +0200

    chore(vite.config): setup proxy for Vercel functions

--- vite.config.ts
+++ vite.config.ts
@@ -9,4 +9,13 @@ export default defineConfig({
     globals: true,
     environment: "jsdom",
   },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 });
