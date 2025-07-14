603f53a4ecc908e25d97126d3ecb74a7597871d9
th3N0m4d <edielton.estrela.dantas@sap.com>
2025-07-14 04:52:43 +0200

    feat: integrate user authentication check in App component with loading state

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import AppRoutes from "./Routes";
 import { Amplify } from "aws-amplify";
-import amplifyconfig from "./amplifyconfiguration.json";
+// import amplifyconfig from "./amplifyconfiguration.json";
+import amplifyconfig from "./amplify-config";
+import { useEffect } from "react";
+import { useAuthStore } from "./hooks";
 
 Amplify.configure(amplifyconfig);
 
 function App() {
+  const { checkUser, loading } = useAuthStore();
+  useEffect(() => {
+    checkUser();
+  }, [checkUser]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <main className="app">
       <AppRoutes />
