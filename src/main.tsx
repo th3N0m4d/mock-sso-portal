2e211005ff72393dedce22fd20bcdbabd4732dd7
th3N0m4d <edielton.estrela.dantas@sap.com>
2025-07-17 08:41:22 +0200

    feat: add ToastContainer for notifications in main application render

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
 
 import App from "./App.tsx";
 
@@ -8,6 +10,7 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <App />
+      <ToastContainer pauseOnHover={false} position="top-center" />
     </BrowserRouter>
   </StrictMode>
 );
